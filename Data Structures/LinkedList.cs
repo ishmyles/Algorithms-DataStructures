8a04bf9044cd761950190e68adb84bcaf5ebad70
Myles-Glenn Gaerlan <m.gaerlan@live.com>
2018-07-01 22:24:17 +1000

    Initial Commit

---
+++ Data Structures/LinkedList.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Threading;
+using System.Transactions;
+
+namespace Data_Structures
+{
+    public class LinkedListNode {
+        public string Data;
+        public LinkedListNode Next;
+
+
+        public LinkedListNode(string data, LinkedListNode next) {
+            this.Data = data;
+            this.Next = null;
+        }
+    }
+
+    public class LinkedList {
+        public LinkedListNode Head;
+
+        public LinkedList() {
+            Head = null;
+        }
+
+        public void AddToEnd(string newData) {
+            LinkedListNode newNode = new LinkedListNode(newData, null);
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+            else
+            {
+                LinkedListNode current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+        
+        public LinkedListNode GetNodeAt(int index) {
+            int count = 0;
+
+            if (index < 0) {
+                return null;
+            }
+            
+            LinkedListNode current = Head;
+            while (count < index) {
+                if (current.Next != null) {
+                    current = current.Next;
+                } else {
+                    return null;
+                }
+                count++;
+            }
+
+            return current;
+        }
+
+        public bool Find(string searchTerm) {
+            LinkedListNode current = Head;
+
+            while (current != null) {
+                if (current.Data == searchTerm) {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the number of nodes in the Linked List
+        /// </summary>
+        /// <returns>int: count</returns>
+        public int Count()
+        {
+            int count = 0;
+
+            if (Head == null)
+                return count;
+            else
+            {
+                LinkedListNode current = Head;
+
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                    count++;
+                }
+                return count;
+            }
+        }
+
+        /// <summary>
+        /// Adds a node to the start of the list.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>success: true</returns>
+        public bool AddToStart(string data) {
+            LinkedListNode newHead = new LinkedListNode(data, null);
+            if (Head == null)
+            {
+                Head = newHead;
+                return false;
+            }
+            else
+            {
+                newHead.Next = Head;
+                Head = newHead;
+                return true;
+            }
+        }
+
+        public bool AddNodeAt(string data, int index) {
+            LinkedListNode newNode = new LinkedListNode(data, null);
+            int count = 0;
+            if (Head == null) {
+                Head = newNode;
+                return false;
+            }
+            else
+            {
+                LinkedListNode current = Head;
+                while (count != (index - 1))
+                {
+                    current = current.Next;
+                    count++;
+                }
+                LinkedListNode tmp = current.Next;
+                current.Next = newNode;
+                newNode.Next = tmp;
+                return true;
+            }
+        }
+
+        public bool DeleteNodeAt(int index) {
+            int count = 0;
+            if (Head == null)
+                return false;
+            else
+            {
+                LinkedListNode current = Head;
+                while(count != (index - 1))
+                {
+                    current = current.Next;
+                    count++;
+                }
+                current.Next = current.Next.Next;
+                return true;
+            }
+        }
+    }
+}
